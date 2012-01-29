e2808c617cb35ed3bacc388154bec4ee04c6ce60
Mark Cole <mark.a.cole@me.com>
2012-01-29 15:30:08 +0000

    added new project

---
+++ abaca/abaca/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  abaca
+//
+//  Created by Mark Cole on 29/01/2012.
+//  Copyright (c) 2012 SeriouslyRevival. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ABCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ABCAppDelegate class]));
+    }
+}
